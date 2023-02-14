89c94f5c8121a96b4403945295bdf366192f89b0
Fahim <writetofahimahammad@gmail.com>
2023-02-14 15:51:28 +0600

    callback function updated

--- callback/callback.js
+++ callback/callback.js
@@ -1,5 +1,5 @@
 function myDisplayer(some) {
-  document.getElementById("demo").innerHTML = some;
+  // document.getElementById("demo").innerHTML = some;
 }
 
 function myCalculator(num1, num2, myCallback) {
@@ -8,3 +8,34 @@ function myCalculator(num1, num2, myCallback) {
 }
 
 myCalculator(5, 5, myDisplayer);
+
+// date 14 feb 2023
+
+function myDisplayer2(result) {
+  console.log(result);
+}
+function calculator(a, b) {
+  return a + b;
+}
+const x = calculator(5, 4);
+myDisplayer2(x);
+// Here we had to call to function which is not the convention
+function myDisplayer3(result) {
+  console.log(result);
+}
+function myClaculator3(a, b) {
+  const x = a + b;
+  myDisplayer3(x);
+}
+myClaculator3(4, 4);
+// here we couldn't stop the calling myDisplayer function
+
+// here comes the callback function
+function display(result) {
+  console.log(result);
+}
+function calculator1(a, b, callback) {
+  const sum = a + b;
+  callback(sum);
+}
+calculator1(7, 8, display);
